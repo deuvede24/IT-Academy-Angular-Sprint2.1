b2a1b98496d30a901a9192c5fff68983b3f732f4
deuvede24 <cdaniela.vd@gmail.com>
2024-04-27 15:18:36 +0200

    changes

--- 1.1_arrow_functions/nivell1/resultados.js
+++ 1.1_arrow_functions/nivell1/resultados.js
@@ -1,9 +1,8 @@
+function mainex1() {
+  //convertiremos la function add(a, b) {return a + b;} en tipo flecha
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  const add = (a, b) => a + b;
 
-function mainex1(){
-//convertiremos la function add(a, b) {return a + b;} en tipo flecha
-let a = Number (document.getElementById("num1").value);
-let b = Number (document.getElementById("num1").value);
-
-const add = (a,b)=> a + b;
-document.getElementById.innerHTML("resultado")=add(a,b);
+  document.getElementById("resultado").innerHTML = add(n1, n2);
 }
