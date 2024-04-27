6b55b49c25dd87ef21e1ce0a74a740f622afac1c
deuvede24 <cdaniela.vd@gmail.com>
2024-04-27 17:36:10 +0200

    nivell1 done

--- 1.1_arrow_functions/nivell1/resultados.js
+++ 1.1_arrow_functions/nivell1/resultados.js
@@ -2,7 +2,31 @@ function mainex1() {
   //convertiremos la function add(a, b) {return a + b;} en tipo flecha
   let n1 = Number(document.getElementById("num1").value);
   let n2 = Number(document.getElementById("num2").value);
-  const add = (a, b) => a + b;
 
-  document.getElementById("resultado").innerHTML = add(n1, n2);
+  document.getElementById("resultado1").innerHTML = add(n1, n2);
+}
+
+const add = (a, b) => a + b;
+
+function mainex2() {
+  document.getElementById("resultado2").innerHTML = random();
+}
+const random = () => Math.random() * 101;
+
+
+function mainex3() {
+  const nombre = document.getElementById("name").value;
+  const persona = new Person(nombre);
+  persona.greet();
+}
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet = () => {
+    console.log(`Hola, ${this.name}`); //saludo por consola
+    document.getElementById("resultado3").innerHTML = `Hola, ${this.name}`; //saludo en pantalla
+  };
 }
