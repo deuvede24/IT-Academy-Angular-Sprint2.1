cbc0e2cfc0cdecafb743a1fb7b07535867c93f3b
deuvede24 <cdaniela.vd@gmail.com>
2024-04-28 18:25:54 +0200

    callbackn1

---
+++ 1.3_call_backs/nivell1/script.js
@@ -0,0 +1,21 @@
+//Ex.1
+function processar(num,doble){
+    doble(num);
+}
+function doble(n){
+    console.log("Resultado del Ex.1= " + (n*2));
+}
+
+processar(8,doble);
+
+//Ex.2
+function calcular(num1,num2,sumar){
+    sumar(num1,num2);
+}
+function sumar(a,b){
+    console.log("Resultado del Ex.2= " + (a+b));
+}
+
+calcular(1,2,sumar);
+
+
