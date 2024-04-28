b136dd650f4098cde40104553494ec43744be62d
deuvede24 <cdaniela.vd@gmail.com>
2024-04-28 10:52:16 +0200

    last

---
+++ 1.1_arrow_functions/nivell2/script.js
@@ -0,0 +1,10 @@
+//Array de números
+const arrayDeNumeros = [1, 2, 3, 4, 5];
+
+//Función flecha que admite un array de número y mediante un loop for lo imprime por consola
+const printNumbers = () => {
+    for (let i = 0; i < arrayDeNumeros.length; i++) {
+        console.log(arrayDeNumeros[i]);
+    }
+}
+
