03227fa2494b3f617cb930399978e61a9b69eee0
deuvede24 <cdaniela.vd@gmail.com>
2024-04-29 11:11:20 +0200

    callbacks n2 done

--- 1.3_call_backs/nivell2/script.js
+++ 1.3_call_backs/nivell2/script.js
@@ -1,17 +1,36 @@
-
-function esperarISaludar(nombre,callback){
-setTimeout(() => {
+//Ex.1
+function esperarISaludar(nombre, callback) {
+  setTimeout(() => {
     callback(nombre);
   }, 2000);
 }
 
 function saludar(nombre) {
-    alert("Hola, " + nombre);
-    console.log("Hola, " + nombre)
+  alert("Hola, " + nombre);
+  console.log("Hola, " + nombre);
 }
 
 function saludarAsincrono() {
-    let nombre = document.getElementById("nom").value;
-    esperarISaludar (nombre, saludar);
+  let nombre = document.getElementById("nom").value;
+  esperarISaludar(nombre, saludar);
 }
 
+//Ex.2
+const arrayElements = [1, 2, 3, 4, 5];
+function processarElements(array, callback) {
+  //Uso de forEach para iterar sobre cada elemento del array
+  /*array.forEach((element) => {
+    callback(element);
+  });*/
+
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    callback(element);
+  }
+}
+function mostrarElements(elemento) {
+  console.log("Elemento: " + (elemento + 2));
+}
+function mainex24() {
+  processarElements(arrayElements, mostrarElements);
+}
