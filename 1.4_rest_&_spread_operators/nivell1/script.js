1adaf02582a121f8f794ed7f28380b2fcbdd6fa5
deuvede24 <cdaniela.vd@gmail.com>
2024-04-29 13:44:33 +0200

    spread operator done

---
+++ 1.4_rest_&_spread_operators/nivell1/script.js
@@ -0,0 +1,7 @@
+function mainex1() {
+  const array1 = [1, 2, 3, 4, 5];
+  const array2 = [6, 7, 8, 9, 10];
+
+  const array3 = [...array1, ...array2];
+  console.log("Array resultado:", array3);
+}
