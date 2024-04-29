3352a76076b217e9b1f7ce3d76e0a109e8feae68
deuvede24 <cdaniela.vd@gmail.com>
2024-04-29 11:29:41 +0200

    cambio html funciones

--- 1.3_call_backs/nivell1/script.js
+++ 1.3_call_backs/nivell1/script.js
@@ -1,21 +1,23 @@
 //Ex.1
-function processar(num,doble){
+function mainex1() {
+  function processar(num, doble) {
     doble(num);
-}
-function doble(n){
-    console.log("Resultado del Ex.1= " + (n*2));
+  }
+  function doble(n) {
+    console.log("Resultado del Ex.1= " + n * 2);
+  }
+  processar(8, doble);
 }
 
-processar(8,doble);
+
 
 //Ex.2
-function calcular(num1,num2,sumar){
-    sumar(num1,num2);
+function mainex2() {
+  function calcular(num1, num2, sumar) {
+    sumar(num1, num2);
+  }
+  function sumar(a, b) {
+    console.log("Resultado del Ex.2= " + (a + b));
+  }
+  calcular(1, 2, sumar);
 }
-function sumar(a,b){
-    console.log("Resultado del Ex.2= " + (a+b));
-}
-
-calcular(1,2,sumar);
-
-
