876ebfac1ed114c99cf008c026e5d8b5041ec653
deuvede24 <cdaniela.vd@gmail.com>
2024-04-30 10:58:53 +0200

    sprea&rest

--- 1.4_rest_&_spread_operators/nivell1/script.js
+++ 1.4_rest_&_spread_operators/nivell1/script.js
@@ -5,3 +5,14 @@ function mainex1() {
   const array3 = [...array1, ...array2];
   console.log("Array resultado:", array3);
 }
+
+function mainex2() {
+  function sum(...numbers) {
+    /*utilizamos reduce para sumar todos los valores entre sí. 
+    total es el varlo acumulado y num el valor del elemento actual 
+    a sumar y el 0 indica inicializmaos el total con este valor.*/
+    return numbers.reduce((total, num) => total + num, 0);
+  }
+
+  console.log("La suma de los elementos es:" + sum(1, 2, 3, 4, 5, 6));
+}
